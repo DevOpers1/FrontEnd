e6d0424e600dbb5276c975bf350e82e3ea6d63fa
DmytroHanyn <dmytro.hanyn.ri.2024@lpnu.ua>
2025-03-31 21:42:28 +0300

    React-native

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const GymScan = () => {
+    return (
+        <LinearGradient colors={['#B8BEE6', '#5C65A1']} style={styles.container}>
+            <Image source={require('./assets/LogoGymscan.png')} style={styles.logo} />
+            
+            <View style={styles.nameContainer}>
+                <Text style={styles.welcomeText}>Welcome to</Text>
+                <Text style={styles.appName}>GymScan</Text>
+            </View>
+            
+            <Text style={styles.lowerName}>Ваш планер тренувань у спортзалі</Text>
+            
+            <TouchableOpacity style={styles.googleBtn}>
+                <Image 
+                    source={{ uri: 'https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg' }}
+                    style={styles.googleBtnImg}
+                />
+                <Text style={styles.googleBtnText}>Continue with Google</Text>
+            </TouchableOpacity>
+        </LinearGradient>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        width: 170,
+        height: 170,
+        marginBottom: 60,
+    },
+    nameContainer: {
+        alignItems: 'center',
+    },
+    welcomeText: {
+        fontSize: 22,
+        color: '#303030',
+        fontWeight: '800',
+    },
+    appName: {
+        fontSize: 48,
+        color: '#011936',
+        fontWeight: '800',
+    },
+    lowerName: {
+        fontSize: 15,
+        color: '#606060',
+        fontWeight: '600',
+        marginTop: 5,
+    },
+    googleBtn: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'black',
+        borderRadius: 10,
+        marginTop: 60,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+    },
+    googleBtnImg: {
+        width: 18,
+        height: 18,
+        marginRight: 10,
+    },
+    googleBtnText: {
+        fontSize: 15,
+        color: 'white',
+        fontWeight: 'bold',
+    },
 });
+
+export default GymScan;
